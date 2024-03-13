5a605c6418a0c8eaee0f0ef67d60e7ef97a99e19
JeremyETMLES <113518025+JeremyETMLES@users.noreply.github.com>
2024-03-13 17:10:19 +0100

    Corrections

---
+++ POBJ_JAR/TestPOBJ_16.01.24/Mobile.java
@@ -0,0 +1,17 @@
+// Auteur: JAR
+// Date: 1.01.24
+
+class Mobile
+{
+	// Attributs
+	protect short posX;
+	protect short posY;
+	protect float vitesseX;
+	protect float vitesseY;
+	protect boolean Vehicule;
+	
+	// Méthodes
+	public Mobile(char carac, byte val){}
+	private float ChoixVitesse(){}
+	public boolean Run(){}
+}
--- POBJ_JAR/TestPOBJ_16.01.24/TestPOBJ_Ex7/TestPOBJ_Ex7/Program.cs
+++ POBJ_JAR/TestPOBJ_16.01.24/TestPOBJ_Ex7/TestPOBJ_Ex7/Program.cs
@@ -6,10 +6,15 @@ namespace EX7
     class Generator
     {
         // Attributs
-        private long frequency;
-        private float amplitude;
+        private ulong frequency;
+        private double amplitude;
         // Méthodes
-        public double CalculateSignalPoints() { }
+        public decimal CalculateSignalPoints() 
+        {
+            decimal varRetour = 0;
+
+            return varRetour;
+        }
         public Generator() { }
     }
     class User
@@ -17,7 +22,12 @@ namespace EX7
         // Attributs
 
         // Méthodes
-        public bool GeneratorConfigure(int freq, float amp, String signalType) { }
+        public bool GeneratorConfigure(ulong freq, double amp, String signalType) 
+        {
+            bool erreur = false;
+
+            return erreur;
+        }
         public User() { }
     }
 }
